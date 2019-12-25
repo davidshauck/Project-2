2d0973fa9c26e877941499f45f2716c09263105a
David Hauck <dhauck@mac.com>
2019-12-25 14:39:13 -0500

    images populating divs upon selection

--- public/game.js
+++ public/game.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
 
-    APIKEY="t3uiacex9b36"
+    // APIKEY="t3uiacex9b36"
 
 let result = {};
 let playerArray = [];
 let playersDraftIndex = 0;
 let playerIdIndex = 0;
 let playerImageIndex = 0;
-// let playerIds = ["19781","20841","12841","19781","20841","12841","19781","20841","12841","19781","20841","12841","20841"];
 let playerIds = [];
+let position = "QB";
+let imagePosition = 1;
 
 let playerId = "";
+
+
+
+// document.getElementById("myImg").src = "assets/images/scaffold.gif";
+
+
+$( ".position-dropdown" ).on( "click", function() {
+    // alert( $( this ).text() );
+    playerArray = [];
+    playerIndex = 0;
+    playerIds = [];
+    playerId = "";
+    playerIdIndex = 0;
+    position = $(this).text();
+    $( ".drop-test" ).empty();
+    console.log("position " + position);
+  
+//   $( ".dropdown-item" ).trigger( "click" );
 
 
 
@@ -18,12 +37,7 @@ let playerId = "";
     // players.sort((a, b) => (a.PlayerID > b.PlayerID) ? 1 : -1);
     // console.log(players);
     
-
-
-    // playerImage();
-    // console.log(fantasyPlayers);
-
-    var playerDropdown = $("<a>");
+let playerDropdown = $("<a>");
 
 
 
@@ -34,19 +48,15 @@ let playerId = "";
             console.log("**************");
 
             for (let i = 0; i < 15; i++) {
-                // console.log(players.ShortName);
-                // playerDropdown.attr("<a>");
                 playerDropdown.attr("<div>");
-                playerDropdown.attr("href", "#");
+                // playerDropdown.attr("href", "#");
                 playerDropdown.addClass("dropdown-item");
-                // playerDropdown.addClass("dd"+i);
+                playerDropdown.addClass("player-dropdown");
                 playerDropdown.attr("data-id", playerArray[i].PlayerID);
                 playerDropdown.attr("id", "dd"+i);
                 playerDropdown.html(playerArray[i].Name + " | $" + playerArray[i].YahooSalary);
                 playerDropdown.appendTo(".drop-test");
-                // getPlayerIds().then(getPlayerInfo).then(function(data) {
-                // $("<img src='" + data.PhotoUrl + "'>").prependTo("#dd"+i);
-                // });
+  
                 getImage(function(data){
                     
                     data = JSON.parse(data);
@@ -59,6 +69,32 @@ let playerId = "";
                     
                 });
             }
+            $( ".player-dropdown" ).on( "click", function() {
+                playerId = ($(this).attr("data-id"));
+                populateImageDiv(function(data){
+                    
+                    data = JSON.parse(data);
+                    // console.log(data.PhotoUrl);
+                    // result.push(data.PhotoUrl)
+                    // console.log(result);
+                    $("#img"+imagePosition).attr("src", data.PhotoUrl);
+                    imagePosition += 1;
+                    console.log(data.PhotoUrl);
+                    console.log(imagePosition);
+
+                    // alert( "success")
+                    // $("<img src='" + data.PhotoUrl + "'>").prependTo("#dd"+i);
+        
+                    
+                });
+        
+        });
+            playerArray = [];
+        });
+
+
+
+        
 
         
                 // playerDropdown.attr("<src>", "https://api.sportsdata.io/v3/nfl/scores/json/Player/19781?key=11e7e6cab8c84674a144d406b28e561d")
@@ -167,14 +203,8 @@ let playerId = "";
         //    });
         // }
 
-    
-    
-    
-
-    var userLocation;
-
     function getPlayerIds() {
-        let position = "QB";
+        // let position = "QB";
         let week = "16";
         let queryUrl = "https://api.sportsdata.io/v3/nfl/stats/json/GameLeagueLeaders/2019REG/" + week +"/" + position + "/FantasyPoints?key=87259770c8654c4aa8d0dd12658e7d93";
 
@@ -214,6 +244,30 @@ let playerId = "";
     // getPlayerIds().then(getPlayerInfo).then(function(data) {
     //   // data from the weather(userLocation).json file is available here
     // });
+
+
+    function populateImageDiv(cb){
+
+            
+        // console.log(playerIdIndex)
+        // console.log(cb);
+        console.log("playerId" + playerId);
+        // console.log(playerId);
+        let playerUrl = "https://api.sportsdata.io/v3/nfl/scores/json/Player/"+playerId+"?key=87259770c8654c4aa8d0dd12658e7d93";
+
+        // console.log(playerIdIndex);
+
+        $.ajax({
+            url: playerUrl,
+            type: "GET",
+            dataType: "text",
+            cache: false,
+            success: function(data){
+                // call the callback passed
+                cb(data);
+            }
+       });
+    }
 
 
 
