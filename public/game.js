961bcf61f67cf03bb8bb90cd30f109d7f18ded20
David Hauck <dhauck@mac.com>
2019-12-26 10:13:14 -0500

    computer team populating randomly. Still need to make sure it doesn't repeat players, check when computer team is complete, decrement budget

--- public/game.js
+++ public/game.js
@@ -6,12 +6,16 @@ let playerArray = [];
 let playerIdIndex = 0;
 let playerIds = [];
 let position = "";
-let imagePosition = 1;
 let splicePosition = 0;
 let imageUrls = [];
 let playerId = "";
+let computerTeam = [];
+let computerPlayer = "";
+let computerBudget = 250;
+let playerBudget = 250;
 
-renderImages();
+renderUserTeam();
+renderComputerTeam();
 
 $( ".position-dropdown" ).on( "click", function() {
     // alert( $( this ).text() );
@@ -23,20 +27,16 @@ $( ".position-dropdown" ).on( "click", function() {
     position = $(this).text();
     $( ".drop-test" ).empty();
     console.log("position " + position);
-  
-//   $( ".dropdown-item" ).trigger( "click" );
-
-
-
     
     // players.sort((a, b) => (a.PlayerID > b.PlayerID) ? 1 : -1);
     // console.log(players);
-    
-let playerDropdown = $("<a>");
-
 
 
         getPlayerIds().then(getPlayerInfo).then(function(data) {
+            computerPlayer = playerArray[Math.floor(Math.random()*playerArray.length)].PlayerID;
+
+
+            let playerDropdown = $("<a>");
 
             for (let i = 0; i < 15; i++) {
                 playerDropdown.attr("<div>");
@@ -50,33 +50,49 @@ let playerDropdown = $("<a>");
                 getImage(function(data){
                     
                     data = JSON.parse(data);
+                    console.log("*************");
+                    console.log(data);
+                    console.log("**************");
+
 
                     $("<img src='" + data.PhotoUrl + "'>").prependTo("#dd"+i);
-        
-                    
+             
                 });
             }
             $( ".player-dropdown" ).on( "click", function() {
                 playerId = ($(this).attr("data-id"));
                 
                 populateImageDiv(function(data){
+
                     
                     data = JSON.parse(data);
-                    // console.log(data.PhotoUrl);
-                    // result.push(data.PhotoUrl)
-                    // console.log(result);
-                    imageUrls.push(data.PhotoUrl);
 
-                    // let arr2 = imageUrls.filter(a => a !== 'e')
-                    renderImages();
-                    // imagePosition += 1;
+   
+                    imageUrls.push({id: data.PlayerID, url: data.PhotoUrl});
+                    // computerTeam.push({id: data.PlayerID, url: data.PhotoUrl});
+
+
+                    renderUserTeam();
+                    // renderComputerTeam();
+ 
                     console.log(imageUrls);
-                    // console.log(imagePosition);
-
-                    // alert( "success")
-                    // $("<img src='" + data.PhotoUrl + "'>").prependTo("#dd"+i);
         
+                });
+                populateComputerTeam(function(data){
+
                     
+                    data = JSON.parse(data);
+
+   
+                    computerTeam.push({id: data.PlayerID, url: data.PhotoUrl});
+                    // computerTeam.push({id: data.PlayerID, url: data.PhotoUrl});
+
+
+                    // renderUserTeam();
+                    renderComputerTeam();
+ 
+                    console.log(computerTeam);
+        
                 });
         
         });
@@ -86,35 +102,29 @@ let playerDropdown = $("<a>");
 
 
            // removes images
-    $("#image-grid").on("click",".images", function(e) {
+    $("#player-image-grid").on("click",".images", function(e) {
         e.preventDefault()
         // imagePosition+=1;
         splicePosition = $(this).attr("data-position");
         let imageSrc = ($(this).attr("src"));
-    console.log("splice " + splicePosition);
-    // console.log(array);
-    // $("#img" + $(this).attr("data-position")).attr("src", "./images/150px.png");
-    // array.splice(removeBtn, 1);
-    // console.log(array);
-    // imageUrls.splice(splicePosition);
-    let imageUrls2 = imageUrls.filter(a => a !== imageSrc); 
-    imageUrls = imageUrls2;
-    console.log("&&&&&&&&&&&&&&&&&")
-    console.log(imageUrls2);
-    console.log("&&&&&&&&&&&&&&&&&")
+        console.log("###############")
+        console.log(imageSrc);
+        console.log("###############")
+
+    // console.log("splice " + splicePosition);
+
+    for( let i = 0; i < imageUrls.length; i++){ 
+        if ( imageUrls[i].url === imageSrc) {
+          imageUrls.splice(i, 1); 
+        }
+     }
+    // let imageUrls2 = imageUrls.url.filter(a => a !== imageSrc); 
+    // imageUrls = imageUrls2;
 
 
-
-    // imageUrls = imageUrls.filter(emp => emp.name.localeCompare(name));
-
-// console.log(employees);
-    // imagePosition -= 1;
     console.log(imageUrls);
-    // updatedImagePosition = imagePosition - 1;
-    // imagePosition = updatedImagePosition;
-    ;
 
-    renderImages();
+    renderUserTeam();
 
 
 
@@ -184,11 +194,6 @@ let playerDropdown = $("<a>");
     }
     
     function getPlayerInfo() {
-        
-        // console.log("------------------");
-        // console.log(playerArray);
-        // console.log("------------------");
-
       return $.ajax("https://api.sportsdata.io/v3/nfl/scores/json/Player/"+playerId+"?key=87259770c8654c4aa8d0dd12658e7d93");
     }
     
@@ -200,14 +205,27 @@ let playerDropdown = $("<a>");
 
     function populateImageDiv(cb){
 
-            
-        // console.log(playerIdIndex)
-        // console.log(cb);
         console.log("playerId" + playerId);
         // console.log(playerId);
         let playerUrl = "https://api.sportsdata.io/v3/nfl/scores/json/Player/"+playerId+"?key=87259770c8654c4aa8d0dd12658e7d93";
 
-        // console.log(playerIdIndex);
+        $.ajax({
+            url: playerUrl,
+            type: "GET",
+            dataType: "text",
+            cache: false,
+            success: function(data){
+                // call the callback passed
+                cb(data);
+            }
+       });
+    };
+
+    function populateComputerTeam(cb){
+
+        console.log("playerId" + computerPlayer);
+        // console.log(playerId);
+        let playerUrl = "https://api.sportsdata.io/v3/nfl/scores/json/Player/"+computerPlayer+"?key=87259770c8654c4aa8d0dd12658e7d93";
 
         $.ajax({
             url: playerUrl,
@@ -223,8 +241,8 @@ let playerDropdown = $("<a>");
 
 
 
-    function renderImages() {
-        $("#image-grid").empty();
+    function renderUserTeam() {
+        $("#player-image-grid").empty();
 
         for (let i = 0; i < ((6 - imageUrls.length) + parseInt(imageUrls.length)); i ++) {
             console.log("image urls length " + imageUrls.length);
@@ -236,14 +254,60 @@ let playerDropdown = $("<a>");
             if (i >= imageUrls.length) {
                 images.attr("src", "./images/150px.png");
             } else {
-                images.attr("src", imageUrls[i]);
+                images.attr("src", imageUrls[i].url);
             }
             images.attr({ width: "33%", padding: "10px" });
-            $("#image-grid").append(images);
+            $("#player-image-grid").append(images);
         };
-        console.log("rendering");
+        console.log("rendering player");
 
         }
+
+        function renderComputerTeam() {
+            $("#computer-image-grid").empty();
+    
+            for (let i = 0; i < ((6 - computerTeam.length) + parseInt(computerTeam.length)); i ++) {
+                console.log("computer team length " + computerTeam.length);
+                let images = $("<img>");
+                images.addClass("images");
+                images.attr("id-comp", "img"+i);
+                images.attr("value", i)
+                images.attr("data-position", i);
+                if (i >= computerTeam.length) {
+                    images.attr("src", "./images/150px.png");
+                } else {
+                    images.attr("src", computerTeam[i].url);
+                }
+                images.attr({ width: "33%", padding: "10px" });
+                $("#computer-image-grid").append(images);
+            };
+            console.log("rendering computer");
+    
+            }
+
+            // function computerPick() {
+            //     populateImageDiv(function(data){
+                    
+            //         data = JSON.parse(data);
+
+            //         let computerPlayer = playerArray[Math.floor(Math.random()*playerArray.length)]
+
+   
+            //         computerTeam.push({id: data.PlayerID, url: data.PhotoUrl});
+
+            //         renderComputerTeam();
+ 
+            //         console.log(imageUrls);
+
+
+
+
+        
+            //     });
+
+            // }
+
+        
 
 
 
